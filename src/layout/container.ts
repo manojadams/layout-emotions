ed010ddf5b7aec41669d02f5365f215829a8e267
manojgetwealthy <manoj.baruah@wealthy.in>
2023-04-01 17:45:39 +0530

    update container

--- src/layout/container.ts
+++ src/layout/container.ts
@@ -1,9 +1,10 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
+    --gap-x: 1.5rem;
     width: 100%;
-    padding-right: calc(var(--bs-gutter-x) * 0.5);
-    padding-left: calc(var(--bs-gutter-x) * 0.5);
+    padding-right: calc(var(--gap-x) * 0.5);
+    padding-left: calc(var(--gap-x) * 0.5);
     margin-right: auto;
     margin-left: auto;
 `;
--- src/layout/row.ts
+++ src/layout/row.ts
@@ -6,6 +6,7 @@ export const Row = styled.div`
     --gap-y: 0;
     display: flex;
     flex-wrap: wrap;
+    width: 100%;
     margin-top: calc(-1 * var(--gap-y));
     margin-right: calc(-0.5 * var(--gap-x));
     margin-left: calc(-0.5 * var(--gap-x));
