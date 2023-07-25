dc665ca1d72103f63fc0f573aa1fbbadfa0b36bb
Manoj Kumar Baruah <manoj.baruah@wealthy.in>
2023-07-25 20:54:10 +0530

    fix #1:fix row space

--- src/layout/row.ts
+++ src/layout/row.ts
@@ -8,12 +8,12 @@ export const Row = styled.div`
     --gap-y: 0;
     display: flex;
     flex-wrap: wrap;
-    width: 100%;
     margin-top: calc(-1 * var(--gap-y));
     margin-right: calc(-0.5 * var(--gap-x));
     margin-left: calc(-0.5 * var(--gap-x));
     > * {
         flex: 0 0 auto;
+        width: 100%;
         box-sizing: border-box;
         max-width: 100%;
         padding-right: calc(var(--gap-x)*.5);
